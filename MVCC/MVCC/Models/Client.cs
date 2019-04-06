02704a45a3907d0a8964ee8d6ea9cc160ec13de1
josernesto <jecm3>
2019-04-05 22:55:03 -0400

    commit mvc

---
+++ MVCC/MVCC/Models/Client.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCC.Models
+{
+    public enum PayType
+    {
+        Cash,
+        CreditCard,
+        GiftCard
+
+    }
+    public class Client
+    {
+        [Key]
+        public int ClientId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int Nit { get; set; }
+        [Required]
+        public PayType Pay { get; set; }
+    }
+}
---
+++ MVCC/MVCC/Models/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+
+namespace MVCC.Models
+{
+    public class DataContext: DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<MVCC.Models.Client> Clients { get; set; }
+    }
+}
